1af42e946b32c7ea5573cdd9bba9423d2f4aa93c
Omar <omar.facchini@studenti.unitn.it>
2024-01-17 18:36:28 +0100

    fixed directory path to access dataset, also started inserting requirements for pipenv, need to insert the ones for cuda

--- emulator_scripts_OFF/emulator_scripts/crf_computer_v2.sh
+++ emulator_scripts_OFF/emulator_scripts/crf_computer_v2.sh
@@ -1,41 +1,64 @@
 #!/bin/bash
 
+# function that rounds a decimal number to nearest int
 round() {
     echo "scale=0; ($1 + 0.5) / 1" | bc
 }
 
-dir_BASE="ff++_shared/FaceForensics++_shared/"  #modifica con tuo path video social shared
+# my path
+dir_BASE="../../../../../../media/mmlab/Datasets_4TB/ff++_shared/FaceForensics++_shared/"
 
-dir_IMMAGINI="/media/SSD_new/FaceForensics++/manipulated_sequences/"  #modifica con tuo path video non shared
+# original path
+# dir_BASE="ff++_shared/FaceForensics++_shared/"  #modifica con tuo path video social shared
 
+# my path
+dir_IMMAGINI="../../../../../../media/mmlab/Datasets_4TB/FaceForensics++/manipulated_sequences/" 
 
+# original path
+# dir_IMMAGINI="/media/SSD_new/FaceForensics++/manipulated_sequences/"  #modifica con tuo path video non shared
+
+# should use the 2 path above, in row 83-84 and 86-87?
+
+# socials used
 socials=(
   "Facebook"
   "YouTube"
 )
 
+#codec used
 codec=(
   "libx264"
 )
 
+# folders of FaceForencics manipulation techniques
+# chosen 3/5 techniques as requested
 main_folders=(
   "FaceShifter"
   "Deepfakes"
-  "Face2Face"
+  #"Face2Face"
   "FaceSwap"
-  "NeuralTextures"
+  #"NeuralTextures"
 )
 
+# 0000.png
 image_name="%04d.png"
 
 tau=1
+
+# stores one-shot crf
 array1=()
+
+# stores crf values tested, in this case 20-51
 array2=()
+
+# stores duration
 array3=()
 
 crf_1_file="crf_1.csv"
 crf_2_file="crf_2.csv"
 duration_file="duration.csv"
+
+# check existence of the files, if exist -> remove
 if [ -e "test.mp4" ]; then
     rm -f "test.mp4"
     echo "File test.mp4 removed."
@@ -52,6 +75,8 @@ if [ -e "$duration_file" ]; then
     rm -f "$duration_file"
     echo "File $duration_file removed."
 fi
+
+# combine socials, techniques and codecs 
 for folder in "${socials[@]}"; do
   crf_1_file="${folder}"/"crf_1.csv"
   crf_2_file="${folder}"/"crf_2.csv"
@@ -62,12 +87,26 @@ for folder in "${socials[@]}"; do
     for mainf in "${main_folders[@]}"; do
       if [ "$folder" = "YouTube" ];
       then
-        FILES="ff++_shared/FaceForensics++_shared"/"${folder}"/"val_youtube"_"${mainf}"/"*.mp4"
-        BASE_PATH="/media/SSD_new/FaceForensics++/manipulated_sequences/""${mainf}""/c23/videos"
+        # get path of videos for the social and technique used
+
+        #my values
+        FILES="${dir_BASE}""${folder}"/"val_youtube"_"${mainf}"/"*.mp4"
+        BASE_PATH="${dir_IMMAGINI}""${mainf}""/c23/videos"
+
+        # original values
+        # FILES="ff++_shared/FaceForensics++_shared"/"${folder}"/"val_youtube"_"${mainf}"/"*.mp4"
+        # BASE_PATH="/media/SSD_new/FaceForensics++/manipulated_sequences/""${mainf}""/c23/videos"
       else
-        FILES="ff++_shared/FaceForensics++_shared"/"${folder}"/"val_facebook"_"${mainf}"/"*.mp4"
-        BASE_PATH="/media/SSD_new/FaceForensics++/manipulated_sequences/""${mainf}""/c23/videos"
+
+        #my values
+        FILES="${dir_BASE}""${folder}"/"val_facebook"_"${mainf}"/"*.mp4"
+        BASE_PATH="${dir_IMMAGINI}""${mainf}""/c23/videos"
+
+        # original values
+        # FILES="ff++_shared/FaceForensics++_shared"/"${folder}"/"val_facebook"_"${mainf}"/"*.mp4"
+        # BASE_PATH="/media/SSD_new/FaceForensics++/manipulated_sequences/""${mainf}""/c23/videos"
       fi
+      # calculate all informations needed on the file usig ffprobe
       for f in $FILES
           do
           frame_ratio=$(ffprobe -v error -select_streams v -of default=noprint_wrappers=1:nokey=1 -show_entries stream=r_frame_rate "$f")
@@ -89,6 +128,7 @@ for folder in "${socials[@]}"; do
           flag_search=1
           echo "$f"
           echo $BASE_PATH/"$(basename $f)"
+          # test different compression rates (20 to 51)
           for i in $(seq 20 51); do  
             ffmpeg -loglevel error -nostdin -i $BASE_PATH/"$(basename $f)" -r "${frame_ratio}" -crf $i -vcodec "${codecs}" -pix_fmt "${pix_fmt}" "test.mp4"
             d=$(ffprobe -v error -select_streams v:0 -show_entries format=bit_rate -of default=noprint_wrappers=1:nokey=1 test.mp4)
